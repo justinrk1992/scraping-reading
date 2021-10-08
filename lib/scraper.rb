42f492346896f6290e606293d3433c14d0a4c353
justinrk1992 <justinrk92@yahoo.com>
2021-10-07 20:42:43 -0500

    commit tree 507a9b8d2f9dc49a997c7c21c6721c01c3ac10fc

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
+require 'awesome_print'
 
 html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri.HTML5(html)
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+     
